164bc3e94f166b7be92bb091115c2ce2edacd329
Osho Wale <90667947+walebee01@users.noreply.github.com>
2022-08-21 10:57:54 +0100

    Update script.js

--- Timer/script.js
+++ Timer/script.js
@@ -14,7 +14,7 @@
         let seconds = newDate.getSeconds();
         let realDate = newDate.getDate();
 
-        //Dom element
+        //To get the Dom
         let date = document.querySelector('.date');
         let dayy = document.querySelector('.dayy');
         let time = document.querySelector('.time');
@@ -39,17 +39,85 @@
             seconds = '0' + seconds;
         }
 
-        (hours > 12) ? am.style.display = 'none': pm.style.display = 'none';
+        (hours < 12) ? pm.style.display = 'none': am.style.display = 'none';
 
         date.innerHTML = `${realDate} - ${monthsYear[month]} - ${year}`;
         dayy.innerHTML = `${daysWeek[day]}`;
         time.innerHTML = `${hours % 12}:${minutes}:${seconds}`;
 
         setTimeout("getTime()", 1000);
+        console.log(hours);
+    }
+
+
+
+    //function for the stop watch
+
+    const startTimer = () => {
+        let totalSeconds = 0;
+        if (!this.interval) {
+            this.interval = setInterval(function() {
+                ++totalSeconds;
+                let hour = Math.floor(totalSeconds / 3600);
+                let minutes = Math.floor((totalSeconds - hour * 3600) / 60);
+                let seconds = totalSeconds - (hour * 3600 + minutes * 60);
+
+                if (hour < 10) {
+                    hour = "0" + hour;
+                }
+                if (minutes < 10) {
+                    minutes = '0' + minutes;
+
+                }
+                if (seconds < 10) {
+                    seconds = '0' + seconds;
+                }
+
+                document.querySelector(".timer").innerHTML = `${hour}:${minutes}:${seconds}`;
+            }, 1000);
+        }
+    }
+
+    function resetTimer() {
+        clearInterval(this.interval);
+        document.querySelector('.timer').innerHTML = "00:00:00";
+        delete this.interval;
+    }
+
+
+    function pauseTimer() {
+        clearInterval(this.interval);
+        delete this.interval;
 
     }
-    
-    //Stopwatch function
+
+    function resumeTimer() {
+        this.startTimer();
+
+    }
+
+
+    document.querySelector('.start').addEventListener('click', () => {
+        startTimer();
+    });
+    document.querySelector('.pause').addEventListener('click', () => {
+        pauseTimer()
+    });
+    document.querySelector('.resume').addEventListener('click', () => {
+        resumeTimer()
+    })
+    document.querySelector('.sound').addEventListener('click', () => {
+        console.log('Sound');
+    })
+    document.querySelector('.reset').addEventListener('click', () => {
+        resetTimer();
+    })
+    document.querySelector('.switch').addEventListener('click', () => {
+        console.log('Switch');
+    })
+    document.querySelector('.something').addEventListener('click', () => {
+        console.log('Something');
+    })
 
 
 }
